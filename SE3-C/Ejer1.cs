1d34f19d97e2989c7bf681e2721226e9a9c3f8a1
giancord <giancord@gmail.com>
2026-04-10 10:23:05 -0500

    subiendo ejercicio 1 en c#

---
+++ SE3-C/Ejer1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class Ejer1
+    {
+
+        static void Main(string[] args)
+        {
+
+            string n, c;
+
+            Console.Write("Ingrese su nombre: ");
+
+            n = Console.ReadLine();
+
+            Console.Write("\nIngrese su carrera: ");
+
+            c = Console.ReadLine();
+
+            Console.WriteLine("\n------ IMPRESIÓN CON + ------");
+            Console.WriteLine(n + ", Bienvenido al curso FA de la carrera " + c);
+
+
+            Console.WriteLine("\n------ IMPRESIÓN CON $ ------");
+            Console.WriteLine($"{n}, Bienvenido al curso FA de la carrera  {c}");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
