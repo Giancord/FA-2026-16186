895247af8dae20fbb1b41c0d053524a1d6ca7387
giancord <giancord@gmail.com>
2026-04-17 10:36:08 -0500

    subiendo ejercicios parte python

---
+++ SE3-P/EJ3.py
@@ -0,0 +1,8 @@
+num1= int(input("Ingrese el primer número: "))
+
+num2=int(input("Ingrese el segundo número: "));
+
+print(f"\nSuma: {num1+num2}")
+print(f"\nResta: {num1 - num2}")
+print(f"\nMultiplicación: {num1 * num2}")
+print(f"\nDivisión: {num1 / num2}")
